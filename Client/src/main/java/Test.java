802c7d9e6391fca7902be38a1a17e31341453a6c
yyf14 <835168932@qq.com>
2016-08-13 20:43:49 +0800

    Startup!

---
+++ Client/src/main/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Daniel on 2016/8/13.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ Client/src/test/java/Test/TestTest.java
@@ -0,0 +1,36 @@
+package Test; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+import static org.junit.Assert.*;
+
+/** 
+* Test Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>08/13/2016</pre> 
+* @version 1.0 
+*/ 
+public class TestTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
