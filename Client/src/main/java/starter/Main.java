e325eba02d87e18266540f7f31be10989448e5c9
yuinii1215 <hq14@software.nju.edu.cn>
2016-08-14 19:47:46 +0800

    basicChanges

---
+++ Client/src/main/java/starter/Main.java
@@ -0,0 +1,20 @@
+package starter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by QiHan on 2016/8/14.
+ */
+public class Main  extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
