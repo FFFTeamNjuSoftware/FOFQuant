b942d68af2cfb4d65a9bc267247dbec50d1517c9
ljh14 <874855266@qq.com>
2016-08-27 21:49:55 +0800

    Revert "add 2 gifs"
    This reverts commit df1a498e6d10604fda11ead7df0f531ee051a102.

--- Client/src/main/java/ui/userUI/buildUI/BuildPanel2UIController.java
+++ Client/src/main/java/ui/userUI/buildUI/BuildPanel2UIController.java
@@ -9,9 +9,6 @@ import ui.util.InitHelper;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by OptimusPrime on 2016/8/27.
@@ -20,35 +17,19 @@ public class BuildPanel2UIController implements Initializable {
 	private BuildPanel2UIController buildPanel2UIController;
 	@FXML
 	private ImageView nextBt2;
-	@FXML
-	private ImageView waiting2;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		this.buildPanel2UIController=this;
 		initBuildFanel2();
-		scheduleTask2();
 	}
 	private void initBuildFanel2(){
 		ImageView[] imageViews = {nextBt2};
 		InitHelper.beautifyImageViews(imageViews);
 	}
-//	10s后执行
-	private void scheduleTask2(){ Runnable runnable = new Runnable() {
-			public void run() {
-				// task to run goes here
-				waiting2.setVisible(false);
-
-			}
-		};
-		ScheduledExecutorService service = Executors
-				.newSingleThreadScheduledExecutor();
-		// 第二个参数为首次执行的延时时间，第三个参数为定时执行的间隔时间
-//		service.scheduleAtFixedRate(runnable, 10, 1, TimeUnit.SECONDS);
-		service.schedule(runnable,10,TimeUnit.SECONDS);
-	}
 	@FXML
 	public void nextBt2Click() {
 		MainUI.getInstance().changeScene("user_guidePanel","buildPanel3");
 	}
+
 
 }
