df1a498e6d10604fda11ead7df0f531ee051a102
ljh14 <874855266@qq.com>
2016-08-27 21:48:48 +0800

    add 2 gifs

--- Client/src/main/java/ui/userUI/buildUI/BuildPanel3UIController.java
+++ Client/src/main/java/ui/userUI/buildUI/BuildPanel3UIController.java
@@ -5,9 +5,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import starter.MainUI;
+import ui.util.InitHelper;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by OptimusPrime on 2016/8/27.
@@ -16,30 +20,35 @@ public class BuildPanel3UIController implements Initializable {
 	private BuildPanel3UIController buildPanel3UIController;
 	@FXML
 	private ImageView nextBt3;
+	@FXML
+	private ImageView waiting3;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		this.buildPanel3UIController=this;
 		initPanel3();
+		scheduleTask3();
+	}
+	//	10s后执行
+	private void scheduleTask3(){ Runnable runnable = new Runnable() {
+		public void run() {
+			// task to run goes here
+			waiting3.setVisible(false);
+
+		}
+	};
+		ScheduledExecutorService service = Executors
+				.newSingleThreadScheduledExecutor();
+		// 第二个参数为首次执行的延时时间，第三个参数为定时执行的间隔时间
+//		service.scheduleAtFixedRate(runnable, 10, 1, TimeUnit.SECONDS);
+		service.schedule(runnable,10, TimeUnit.SECONDS);
 	}
 	private void initPanel3(){
 		ImageView[] imageViews = {nextBt3};
-		for (int i=0;i < imageViews.length; i++) {
-			ImageView tempImage=imageViews[i];
-			tempImage.addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
-				tempImage.setStyle("-fx-background-color: #23a3f3; -fx-opacity:0.5");
-			});
-
-			tempImage.addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent e) -> {
-				tempImage.setStyle("-fx-background-color: #1F77B9;  -fx-opacity:0.5");
-			});
-
-			tempImage.addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
-				tempImage.setStyle("-fx-background-color: transparent;");
-			});
-		}
+		InitHelper.beautifyImageViews(imageViews);
 	}
 	@FXML
 	public void nextBt3Click() {
 		MainUI.getInstance().changeScene("user_guidePanel","buildPanel4");
 	}
+
 }
