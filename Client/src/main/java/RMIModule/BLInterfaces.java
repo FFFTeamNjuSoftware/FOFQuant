662731238f78b7717b76bb0c9ace7dec0c7ca149
ljh14 <874855266@qq.com>
2016-08-28 22:21:22 +0800

    just for sync

--- Client/src/main/java/RMIModule/BLInterfaces.java
+++ Client/src/main/java/RMIModule/BLInterfaces.java
@@ -1,6 +1,7 @@
 package RMIModule;
 
 import bl.*;
+import bl.fof.FOFProfitAnalyseLogic;
 import config.RMIConfig;
 import org.dom4j.DocumentException;
 
@@ -14,60 +15,64 @@ import java.rmi.RemoteException;
  */
 public class BLInterfaces {
 
-    private static BaseInfoLogic baseInfoLogic;
-    private static InvestmentPortfolioLogic investmentPortfolioLogic;
-    private static MarketLogic marketLogic;
-    private static ProfitFeatureLogic profitFeatureLogic;
-    private static RiskFeatureLogic riskFeature;
-    private static UserLogic userLogic;
+	private static BaseInfoLogic baseInfoLogic;
+	private static InvestmentPortfolioLogic investmentPortfolioLogic;
+	private static MarketLogic marketLogic;
+	private static ProfitFeatureLogic profitFeatureLogic;
+	private static RiskFeatureLogic riskFeature;
+	private static UserLogic userLogic;
+	private static FOFProfitAnalyseLogic fofProfitAnalyseLogic;
 
-    /**
-     *
-     * @throws DocumentException
-     */
-    public static void netStart() throws DocumentException {
-        RMIConfig config = RMIConfig.getInstance();
-        String url = "rmi://" + config.getIpAddress() + ":" + config.getPort() + "/";
-        try {
-            baseInfoLogic = (BaseInfoLogic) Naming.lookup(url + BaseInfoLogic.class.getSimpleName());
-            investmentPortfolioLogic = (InvestmentPortfolioLogic) Naming.lookup
-                    (url + InvestmentPortfolioLogic.class.getSimpleName());
-            marketLogic = (MarketLogic) Naming.lookup(url + MarketLogic.class.getSimpleName());
-            profitFeatureLogic = (ProfitFeatureLogic) Naming.lookup(url + ProfitFeatureLogic.class
-                    .getSimpleName());
-            riskFeature = (RiskFeatureLogic) Naming.lookup(url + RiskFeatureLogic.class.getSimpleName());
-            userLogic = (UserLogic) Naming.lookup(url + UserLogic.class.getSimpleName());
-        } catch (NotBoundException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * @throws DocumentException
+	 */
+	public static void netStart() throws DocumentException {
+		RMIConfig config = RMIConfig.getInstance();
+		String url = "rmi://" + config.getIpAddress() + ":" + config.getPort() + "/";
+		try {
+			baseInfoLogic = (BaseInfoLogic) Naming.lookup(url + BaseInfoLogic.class.getSimpleName());
+			investmentPortfolioLogic = (InvestmentPortfolioLogic) Naming.lookup
+					(url + InvestmentPortfolioLogic.class.getSimpleName());
+			marketLogic = (MarketLogic) Naming.lookup(url + MarketLogic.class.getSimpleName());
+			profitFeatureLogic = (ProfitFeatureLogic) Naming.lookup(url + ProfitFeatureLogic.class
+					.getSimpleName());
+			riskFeature = (RiskFeatureLogic) Naming.lookup(url + RiskFeatureLogic.class.getSimpleName());
+			userLogic = (UserLogic) Naming.lookup(url + UserLogic.class.getSimpleName());
+			fofProfitAnalyseLogic = (FOFProfitAnalyseLogic) Naming.lookup(url + FOFProfitAnalyseLogic.class.getSimpleName());
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static BaseInfoLogic getBaseInfoLogic() {
-        return baseInfoLogic;
-    }
+	public static BaseInfoLogic getBaseInfoLogic() {
+		return baseInfoLogic;
+	}
 
-    public static InvestmentPortfolioLogic getInvestmentPortfolioLogic() {
-        return investmentPortfolioLogic;
-    }
+	public static InvestmentPortfolioLogic getInvestmentPortfolioLogic() {
+		return investmentPortfolioLogic;
+	}
 
-    public static MarketLogic getMarketLogic() {
-        return marketLogic;
-    }
+	public static MarketLogic getMarketLogic() {
+		return marketLogic;
+	}
 
-    public static ProfitFeatureLogic getProfitFeatureLogic() {
-        return profitFeatureLogic;
-    }
+	public static ProfitFeatureLogic getProfitFeatureLogic() {
+		return profitFeatureLogic;
+	}
 
-    public static RiskFeatureLogic getRiskFeature() {
-        return riskFeature;
-    }
+	public static RiskFeatureLogic getRiskFeature() {
+		return riskFeature;
+	}
 
-    public static UserLogic getUserLogic() {
-        return userLogic;
-    }
+	public static UserLogic getUserLogic() {
+		return userLogic;
+	}
 
+	public static FOFProfitAnalyseLogic getFofProfitAnalyseLogic() {
+		return fofProfitAnalyseLogic;
+	}
 }
--- Client/src/main/java/ui/userUI/portfolioManagementUI/Analysis2Controller.java
+++ Client/src/main/java/ui/userUI/portfolioManagementUI/Analysis2Controller.java
@@ -2,16 +2,26 @@ package ui.userUI.portfolioManagementUI;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 /**
  * Created by OptimusPrime on 2016/8/28.
  */
 public class Analysis2Controller implements Initializable {
+	@FXML
+	private ComboBox<Date> startCb,endCb;
+	@FXML
+	private Label startDateLb,endDateLb,combinationLb1,combinationLb2;
+	@FXML
+	private ImageView combinationImage1,combinationImage2;
 //	@FXML
-//	private ComboBox<Date>
+//	private TableColum<>
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
