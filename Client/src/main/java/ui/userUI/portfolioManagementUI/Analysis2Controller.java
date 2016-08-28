17de3e8432815faf110093cac17bef328e4ad616
ljh14 <874855266@qq.com>
2016-08-28 21:38:55 +0800

    analysis2.fxml

---
+++ Client/src/main/java/ui/userUI/portfolioManagementUI/Analysis2Controller.java
@@ -0,0 +1,19 @@
+package ui.userUI.portfolioManagementUI;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by OptimusPrime on 2016/8/28.
+ */
+public class Analysis2Controller implements Initializable {
+//	@FXML
+//	private ComboBox<Date>
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+}
