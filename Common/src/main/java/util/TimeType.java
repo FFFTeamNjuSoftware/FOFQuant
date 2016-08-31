56b7eb04c336f415742c298c2a9fb19db13dafbd
ljh14 <874855266@qq.com>
2016-08-31 13:17:19 +0800

    just to get interface code

--- Common/src/main/java/util/TimeType.java
+++ Common/src/main/java/util/TimeType.java
@@ -1,9 +1,28 @@
 package util;
 
+import java.sql.Time;
+
 /**
  * Created by Daniel on 2016/8/18.
  */
 public enum TimeType {
-    ONE_MONTH, THREE_MONTH, SIS_MONTH, ONE_YEAR, THREE_YEAR, FIVE_YEAR, SIN_THIS_YEAR,
-    SINCE_ESTABLISH
+    ONE_MONTH("一个月"), THREE_MONTH("三个月"), SIS_MONTH(""), ONE_YEAR("一年"), THREE_YEAR("三年"), FIVE_YEAR("五年"), SIN_THIS_YEAR(""),
+    SINCE_ESTABLISH("建立至今");
+    private String value;
+    private TimeType(String value){
+
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
