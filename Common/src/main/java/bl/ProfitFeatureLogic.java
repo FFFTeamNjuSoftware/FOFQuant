f4f0656d35460912874551d51ba9205005d4a3ed
yyf <835168932@qq.com>
2016-09-01 15:45:29 +0800

    1

--- Common/src/main/java/bl/ProfitFeatureLogic.java
+++ Common/src/main/java/bl/ProfitFeatureLogic.java
@@ -51,8 +51,22 @@ public interface ProfitFeatureLogic extends Remote {
      */
     public double getEnsembleAveProfitRate(String code) throws RemoteException, ObjectNotFoundException;
 
+    /**
+     * 获得风险收益指数
+     * @param code
+     * @return
+     * @throws RemoteException
+     * @throws ObjectNotFoundException
+     */
     RiskProfitIndex getRiskProfitIndex(String code) throws RemoteException,ObjectNotFoundException;
 
+    /**
+     * 获得投资风格分析
+     * @param code
+     * @return
+     * @throws RemoteException
+     * @throws ObjectNotFoundException
+     */
     InvestStyleAnalyse getInvestStyleAnalyse(String code) throws RemoteException,
             ObjectNotFoundException;
 }
